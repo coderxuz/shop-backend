61b6d98c85c41279d1a413adabcaa097f336e690
ongarbaevsultanbek09 <ongarbaevsultanbek422@gmail.com>
2024-10-30 16:49:37 +0500

    added1

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ from app.api.products import router as prods
 from app.api.basket import router as basket
 
 import uvicorn
+
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173", 
-]
+origins = ["http://localhost:5173", "http://192.168.1.15:5173"]
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,14 +23,17 @@ app.add_middleware(
     allow_headers=["*"],  # Barcha headerlar
 )
 
+
 @app.get("/")
 async def read_root(request: Request):
     # URL ni olish
     url = f"{request.url.scheme}://{request.url.netloc}"
     return {"message": "Hello World", "url": url}
+
+
 app.include_router(login_router)
 app.include_router(images)
 app.include_router(prods)
 app.include_router(basket)
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='192.168.1.15')
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="192.168.1.15")
